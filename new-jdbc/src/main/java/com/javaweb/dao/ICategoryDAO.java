75394db542622eb29bf73558fde83b30f6856036
Trần Ngọc Hải <haj12304@gmail.com>
2022-05-17 16:48:25 +0700

    new commit

---
+++ new-jdbc/src/main/java/com/javaweb/dao/ICategoryDAO.java
@@ -0,0 +1,9 @@
+package com.javaweb.dao;
+
+import java.util.List;
+
+import com.javaweb.model.CategoryModel;
+
+public interface ICategoryDAO extends GenericDAO<CategoryModel>{
+	List<CategoryModel> findAll();
+}
---
+++ new-jdbc/src/main/java/com/javaweb/dao/impl/CategoryDAO.java
@@ -0,0 +1,14 @@
+package com.javaweb.dao.impl;
+import java.util.List;
+import com.javaweb.dao.ICategoryDAO;
+import com.javaweb.mapper.CategoryMapper;
+import com.javaweb.model.CategoryModel;
+
+public class CategoryDAO extends AbstractDAO<CategoryModel> implements ICategoryDAO {
+
+	@Override
+	public List<CategoryModel> findAll() {
+		String sql = "SELECT * FROM category";
+		return query(sql, new CategoryMapper());
+	}
+}
